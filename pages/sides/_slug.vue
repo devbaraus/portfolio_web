95c5ada77da16fa14dee32b70a6f91a58d1d844c
Bruno de Araujo Alves <devbaraus@gmail.com>
2020-08-17 02:49:14 -0300

    Update _slug.vue

--- pages/sides/_slug.vue
+++ pages/sides/_slug.vue
@@ -44,6 +44,7 @@ import Icon from '@/components/Icon'
 import markdown from '@/utils/markdown'
 import Button from '@/components/Button'
 export default {
+scrollToTop: true, 
   name: 'slug',
   components: { Button, Icon },
   async asyncData({ route, $axios }) {
